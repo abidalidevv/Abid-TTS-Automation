17d3f22b6db6196a441ede7baa6825542056e2ed
abidmmp <abidmmp99@gmail.com>
2017-04-02 10:13:00 +0500

    Extract config constants

--- core_logic.py
+++ core_logic.py
@@ -267,3 +267,4 @@
 // Add logging statements - 2023-06-01 10:11:00
 // Add retry mechanism - 2021-04-01 10:07:00
 // Fix string formatting issue - 2021-05-26 10:03:00
+// Extract config constants - 2017-04-02 10:13:00
