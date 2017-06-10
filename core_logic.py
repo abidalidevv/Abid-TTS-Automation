7e28eb142213a964095d73196db06d9ef805eecd
abidmmp <abidmmp99@gmail.com>
2017-06-10 10:01:00 +0500

    Add missing return statement

--- core_logic.py
+++ core_logic.py
@@ -235,3 +235,4 @@
 // Extract config constants - 2018-06-11 10:13:00
 // Clean up imports - 2023-04-30 10:09:00
 // Add default parameter values - 2024-03-12 10:05:00
+// Add missing return statement - 2017-06-10 10:01:00
