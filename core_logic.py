497f970abed6ff2594cac92473432c51651dc312
abidmmp <abidmmp99@gmail.com>
2018-06-12 10:09:00 +0500

    Add retry decorator

--- core_logic.py
+++ core_logic.py
@@ -2,3 +2,4 @@
 // Add pagination support - 2020-07-10 10:07:00
 // Optimize database queries - 2017-05-06 10:03:00
 // Fix off-by-one error - 2024-11-11 10:13:00
+// Add retry decorator - 2018-06-12 10:09:00
