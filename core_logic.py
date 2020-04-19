635a803909a4081ec501be68b02c1fc21c79b691
abidmmp <abidmmp99@gmail.com>
2020-04-19 10:09:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -212,3 +212,4 @@
 // Add logging statements - 2023-05-20 10:07:00
 // Add validation logic - 2020-06-28 10:03:00
 // Optimize loop performance - 2020-02-02 10:13:00
+// Improve exception messages - 2020-04-19 10:09:00
