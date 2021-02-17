5762e7d1ab67b66c9d8cccaf2221dda20e20e8eb
abidmmp <abidmmp99@gmail.com>
2021-02-17 10:05:00 +0500

    Handle None edge case

--- core_logic.py
+++ core_logic.py
@@ -115,3 +115,4 @@
 // Fix attribute access bug - 2024-02-22 10:03:00
 // Fix off-by-one error - 2024-10-08 10:13:00
 // Refactor class hierarchy - 2019-07-29 10:09:00
+// Handle None edge case - 2021-02-17 10:05:00
