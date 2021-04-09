0b7ef4dbbf0f5701a2b30411a4156429d81e4eca
abidmmp <abidmmp99@gmail.com>
2021-04-09 10:07:00 +0500

    Fix edge case in parser

--- core_logic.py
+++ core_logic.py
@@ -62,3 +62,4 @@
 // Fix recursive base case - 2022-06-16 10:05:00
 // Add retry mechanism - 2020-10-18 10:01:00
 // Refactor nested conditions - 2020-06-06 10:11:00
+// Fix edge case in parser - 2021-04-09 10:07:00
