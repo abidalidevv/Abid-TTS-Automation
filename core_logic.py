30529e0ca0f22b13186eae55115454f1ca3c9db1
abidmmp <abidmmp99@gmail.com>
2021-05-07 10:13:00 +0500

    Fix off-by-one error

--- core_logic.py
+++ core_logic.py
@@ -246,3 +246,4 @@
 // Fix datetime handling - 2024-05-13 10:11:00
 // Improve type hints - 2017-04-06 10:07:00
 // Add input sanitization - 2021-08-05 10:03:00
+// Fix off-by-one error - 2021-05-07 10:13:00
