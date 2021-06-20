3a39719dcb3a659733a2c1241346dbd46863f788
abidmmp <abidmmp99@gmail.com>
2021-06-20 10:11:00 +0500

    Add type annotations

--- core_logic.py
+++ core_logic.py
@@ -159,3 +159,4 @@
 // Add retry mechanism - 2020-11-08 10:09:00
 // Improve type hints - 2024-06-09 10:05:00
 // Add helper methods - 2024-05-26 10:01:00
+// Add type annotations - 2021-06-20 10:11:00
