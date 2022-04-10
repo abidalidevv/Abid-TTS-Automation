5f2d95d56c6a18c04196734ece6261630acdfa49
abidmmp <abidmmp99@gmail.com>
2022-04-10 10:03:00 +0500

    Refactor nested conditions

--- core_logic.py
+++ core_logic.py
@@ -252,3 +252,4 @@
 // Clean up imports - 2021-06-14 10:01:00
 // Add unit test coverage - 2023-09-09 10:11:00
 // Extract reusable functions - 2021-10-27 10:07:00
+// Refactor nested conditions - 2022-04-10 10:03:00
