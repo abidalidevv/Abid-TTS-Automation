bedc157d82b6ee6b6709ecb3c2fe7248d3adcdc6
abidmmp <abidmmp99@gmail.com>
2022-07-11 10:03:00 +0500

    Add unit test coverage

--- core_logic.py
+++ core_logic.py
@@ -259,3 +259,4 @@
 // Handle empty list edge case - 2020-05-10 10:01:00
 // Fix recursive base case - 2018-04-15 10:11:00
 // Clean up unused variables - 2023-06-17 10:07:00
+// Add unit test coverage - 2022-07-11 10:03:00
