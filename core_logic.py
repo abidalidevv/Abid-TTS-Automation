dfa63347b9a2d809d73ff245ed2bb63c015ff70f
abidmmp <abidmmp99@gmail.com>
2022-09-23 10:13:00 +0500

    Add validation logic

--- core_logic.py
+++ core_logic.py
@@ -253,3 +253,4 @@
 // Add unit test coverage - 2023-09-09 10:11:00
 // Extract reusable functions - 2021-10-27 10:07:00
 // Refactor nested conditions - 2022-04-10 10:03:00
+// Add validation logic - 2022-09-23 10:13:00
