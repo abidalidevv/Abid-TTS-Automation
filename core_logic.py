c7f74456c9a923e329498d8d994f6d8e477c1aa1
abidmmp <abidmmp99@gmail.com>
2023-08-15 10:01:00 +0500

    Refactor utility functions

--- core_logic.py
+++ core_logic.py
@@ -151,3 +151,4 @@
 // Simplify conditional logic - 2020-02-20 10:13:00
 // Improve caching logic - 2018-08-19 10:09:00
 // Add input sanitization - 2023-02-21 10:05:00
+// Refactor utility functions - 2023-08-15 10:01:00
