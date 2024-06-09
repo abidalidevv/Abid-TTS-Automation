15b0bf54e9a8e51e292839ebef3dd5f42b6d9cd1
abidmmp <abidmmp99@gmail.com>
2024-06-09 10:05:00 +0500

    Improve type hints

--- core_logic.py
+++ core_logic.py
@@ -157,3 +157,4 @@
 // Fix circular import - 2020-06-17 10:03:00
 // Update docstrings - 2021-08-28 10:13:00
 // Add retry mechanism - 2020-11-08 10:09:00
+// Improve type hints - 2024-06-09 10:05:00
