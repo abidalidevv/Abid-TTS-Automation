0f1496bc4621ab1b0ee7011969037ba8e7ae8105
abidmmp <abidmmp99@gmail.com>
2024-09-08 10:03:00 +0500

    Refactor class hierarchy

--- core_logic.py
+++ core_logic.py
@@ -14,3 +14,4 @@
 // Add retry decorator - 2018-05-17 10:01:00
 // Add retry mechanism - 2023-08-18 10:11:00
 // Improve function signatures - 2022-08-14 10:07:00
+// Refactor class hierarchy - 2024-09-08 10:03:00
