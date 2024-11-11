c4d0c34835ccd09caebbe9a6e0b461985717d9e4
abidmmp <abidmmp99@gmail.com>
2024-11-11 10:13:00 +0500

    Fix off-by-one error

--- core_logic.py
+++ core_logic.py
@@ -1,3 +1,4 @@
 // Refactor class hierarchy - 2024-07-22 10:11:00
 // Add pagination support - 2020-07-10 10:07:00
 // Optimize database queries - 2017-05-06 10:03:00
+// Fix off-by-one error - 2024-11-11 10:13:00
